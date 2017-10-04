85987141383a6747eee572eaa6c7d9474ca5fc25
coryb08 <douglasb2008@gmail.com>
2017-10-04 18:49:58 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require "pry"
 
+def my_collect(arr)
+  i = 0
+  newArr = []
+  while i < arr.length
+    # block_given?
+    # yield(arr[i])
 
+    # arr[i]
+    newArr.push(yield(arr[i]))
+    # arr[i]
+    i += 1
+  end
+  return newArr
+end
